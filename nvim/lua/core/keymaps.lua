4e2158ec496700f3753c0a1090c8cccc53d30470
Mat Morris <10860014+mathewmorris@users.noreply.github.com>
2023-09-16 01:25:15 -0500

    Blinking block cursor for everything

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -20,7 +20,7 @@ vim.opt.expandtab = true
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
 -- blinking cursor
-vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.guicursor = "n-v-c-i-ci-ve-r-cr-o:block,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- line numbers
 vim.opt.nu = true
