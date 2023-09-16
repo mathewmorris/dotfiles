b1471a288b29f22202f38b0823b8d5eeb1ffc0e3
Mat Morris <10860014+mathewmorris@users.noreply.github.com>
2023-09-15 20:23:14 -0500

    adds lsp

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -37,5 +37,20 @@ return require('packer').startup(function(use)
     }
 
 	require('nvim_comment').setup()
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
 end)
 
