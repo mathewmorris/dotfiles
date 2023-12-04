06917ded56e99e3bb9e1e926764c80fcd1b2c195
Mat Morris <10860014+mathewmorris@users.noreply.github.com>
2023-12-04 07:43:29 -0600

    remove debugging for now

--- nvim/lua/smoose/plugins.lua
+++ nvim/lua/smoose/plugins.lua
@@ -46,19 +46,10 @@ return require('packer').startup(function(use)
       {'williamboman/mason.nvim'},
       {'williamboman/mason-lspconfig.nvim'},
 
-     -- Autocompletion
+      -- Autocompletion
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'L3MON4D3/LuaSnip'},
-    }
-  }
-
-  use 'jbyuki/one-small-step-for-vimkind'
-  use 'theHamsta/nvim-dap-virtual-text'
-  use {
-    'rcarriga/nvim-dap-ui',
-    requires = {
-      {'mfussenegger/nvim-dap'},
     }
   }
 
